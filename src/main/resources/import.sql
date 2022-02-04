071bd9257b8741a6bf00c393cfa26f2937ecbfc1
Yassine EL GHIZI <yassine.ghizi2@gmail.com>
2022-02-04 14:00:51 +0100

    backEndFinished

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into categories(designation) values('fruits');
+insert into categories(designation) values('drinks');
+insert into categories(designation) values('vegetables');
+insert into categories(designation) values('meats');
